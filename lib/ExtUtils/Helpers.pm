c73d755482d57bfc04d6657255abc506a128c228
Leon Timmermans <fawaka@gmail.com>
2011-04-19 21:06:12 +0000

    Import of LEONT/ExtUtils-Helpers-0.004 from CPAN.
    gitpan-cpan-distribution: ExtUtils-Helpers
    gitpan-cpan-version:      0.004
    gitpan-cpan-path:         LEONT/ExtUtils-Helpers-0.004.tar.gz
    gitpan-cpan-author:       LEONT
    gitpan-cpan-maturity:     released

--- lib/ExtUtils/Helpers.pm
+++ lib/ExtUtils/Helpers.pm
@@ -1,15 +1,17 @@
 package ExtUtils::Helpers;
 BEGIN {
-  $ExtUtils::Helpers::VERSION = '0.003';
+  $ExtUtils::Helpers::VERSION = '0.004';
 }
 use strict;
 use warnings;
 use Exporter 5.57 'import';
 
-use File::Basename;
+use File::Basename qw/basename dirname/;
+use File::Path qw/mkpath/;
 use File::Spec::Functions qw/splitpath splitdir canonpath/;
+use Pod::Man;
 
-our @EXPORT_OK = qw/build_script make_executable split_like_shell man1_pagename man3_pagename/;
+our @EXPORT_OK = qw/build_script make_executable split_like_shell man1_pagename manify man3_pagename/;
 
 sub _make_executable {
   # Perl's chmod() is mapped to useful things on various non-Unix
@@ -212,7 +214,7 @@ sub build_script {
 
 sub man1_pagename {
 	my $filename = shift;
-	return basename($filename).".1";
+	return basename($filename).'.1';
 }
 
 my %separator = (
@@ -229,7 +231,16 @@ sub man3_pagename {
 	my @dirs = grep { length } splitdir($dirs);
 	shift @dirs if $dirs[0] eq 'lib';
 	my $separator = $separator{$^O} || '::';
-	return join $separator, @dirs, "$file.3";
+	return join $separator, @dirs, "$file.3pm";
+}
+
+sub manify {
+	my ($input_file, $output_file, $section, $opts) = @_;
+	my $dirname = dirname($output_file);
+	mkpath($dirname, $opts->{verbose}) if not -d $dirname;
+	Pod::Man->new(section => $section)->parse_from_file($input_file, $output_file);
+	print "Manifying $output_file\n" if $opts->{verbose} > 0;
+	return;
 }
 
 # ABSTRACT: Various portability utilities for module builders
@@ -245,7 +256,7 @@ ExtUtils::Helpers - Various portability utilities for module builders
 
 =head1 VERSION
 
-version 0.003
+version 0.004
 
 =head1 SYNOPSIS
 
@@ -280,6 +291,10 @@ Returns the man page filename for a script.
 =head2 man3_pagename($filename)
 
 Returns the man page filename for a Perl library.
+
+=head2 manify($input_filename, $output_file, $section, $opts)
+
+Create a manpage for the script in C<$input_filename> as C<$output_file> in section C<$section>
 
 =head1 AUTHORS
 
